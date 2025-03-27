ec73c8245b8fd87a8028a0f361b9375964ea87db
Pro <oscod@mail.ru>
2025-03-27 20:00:57 +0300

    Create module main, product and category

--- src/category.py
+++ src/category.py
@@ -1,7 +1,10 @@
+from src.product import Product
+
+
 class Category:
     name: str
     description: str
-    products: list[object]
+    # products: list[object]
 
     category_count = 0
     product_count = 0
@@ -9,7 +12,27 @@ class Category:
     def __init__(self, name: str, description: str, products: list[object]) -> None:
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
 
         Category.category_count += 1
         Category.product_count += len(products) if products else 0
+
+    def add_product(self, product_obj: Product) -> None:
+        self.__products.append(product_obj)
+        Category.product_count += 1
+
+    @property
+    def products(self) -> str:
+        product_str = ''
+        for product in self.__products:
+            product_str += f'{product.name}, {round(product.price, 2)} руб. Остаток: {product.quantity} шт.\n'
+        return product_str
+
+    @products.setter
+    def products(self, product_obj: Product) -> None:
+        self.__products.append(product_obj)
+        Category.product_count += 1
+
+    @property
+    def list_of_products(self) -> list[object]:
+        return self.__products
--- src/product.py
+++ src/product.py
@@ -1,11 +1,30 @@
 class Product:
     name: str
     description: str
-    price: float
+    # price: float
     quantity: int
 
     def __init__(self, name: str, description: str, price: float, quantity: int) -> None:
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+    @classmethod
+    def new_product(cls, new_product: dict):
+        return cls(new_product['name'], new_product['description'], new_product['price'], new_product['quantity'])
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, new_product_price):
+        if new_product_price > 0:
+            if self.__price > new_product_price:
+                if input('Вы понижаете цену! Продолжить? (y/n)') == 'n':
+                    print('Цена не изменена.')
+                    return
+            self.__price = new_product_price
+        else:
+            print('Цена не должна быть нулевая или отрицательная')
