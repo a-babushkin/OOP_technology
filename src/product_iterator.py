c905e0a3946b50314153420c548ba44670a92430
Pro <oscod@mail.ru>
2025-03-31 14:53:19 +0300

    Update tests coverage

--- src/product_iterator.py
+++ src/product_iterator.py
@@ -21,22 +21,22 @@ class ProductIterator:
             raise StopIteration
 
 
-if __name__ == "__main__":
-    product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
-    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
-    product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
-    category1 = Category(
-        "Смартфоны",
-        "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
-        [product1, product2, product3]
-    )
-
-    iterator = ProductIterator(category1)
-
-    for item in iterator:
-        print(item)
-
-    print('========================')
-
-    for item in iterator:
-        print(item)
+# if __name__ == "__main__":
+#     product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+#     product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+#     product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+#     category1 = Category(
+#         "Смартфоны",
+#         "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+#         [product1, product2, product3]
+#     )
+#
+#     iterator = ProductIterator(category1)
+#
+#     for item in iterator:
+#         print(item)
+#
+#     print('========================')
+#
+#     for item in iterator:
+#         print(item)
